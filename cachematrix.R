ea83645ece507566d063d7b793945562cee67207
Connor Claypool <ctclaypool@protonmail.ch>
2018-05-26 14:35:27 +0100

    Fix indentation

--- cachematrix.R
+++ cachematrix.R
@@ -5,17 +5,17 @@
 ## set or get a matrix or its cached inverse
 
 makeCacheMatrix <- function(X = matrix()) {
-  I <- NULL
-  set <- function(Y) {
-    X <<- Y
-    I <<- NULL
-  }
-  get <- function() X
-  setInverse <- function(inverse) I <<- inverse
-  getInverse <- function() I
-  list(set = set, get = get,
-       setInverse = setInverse,
-       getInverse = getInverse)
+    I <- NULL
+    set <- function(Y) {
+        X <<- Y
+        I <<- NULL
+    }
+    get <- function() X
+    setInverse <- function(inverse) I <<- inverse
+    getInverse <- function() I
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
@@ -23,18 +23,17 @@ makeCacheMatrix <- function(X = matrix()) {
 ## retrieving the cached value if it exists
 
 cacheSolve <- function(X, ...) {
-  I <- X$getInverse()
-  # Return cached inverse if it is not null
-  if(!is.null(I)) {
-    message("using cached data")
-    return(I)
-  # Calculate the inverse and cache it if there is
-  # no cached value
-  } else {
-    data <- X$get()
-    # Solve returns the inverse of the matrix
-    I <- solve(data, ...)
-    X$setInverse(I)
-    return(I)
-  }
+    I <- X$getInverse()
+    # Return cached inverse if it is not null
+    if(!is.null(I)) {
+        message("using cached data")
+        return(I)
+        # Calculate the inverse and store it if there is no cached value
+    } else {
+        data <- X$get()
+        # Solve calculates the inverse of the matrix
+        I <- solve(data, ...)
+        X$setInverse(I)
+        return(I)
+    }
 }
